a583b1232df47bd83ee4f966b71793659202cc35
andresgranizo <53346123+andresgranizo@users.noreply.github.com>
2021-08-16 17:07:17 -0500

    Update WeatherController.php
    Add the weather’s information for a given city can’t be found

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
 
+
 class WeatherController extends Controller
 {
     public function ShowWeather ($city)
     {
         $apiKey = config('services.apiKey.key');
         $response = Http::get("https://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=metric");
-        return view('weatherview', ['weather' => $response->json()]);
+        if($response['cod'] == 200) {
+            return view('weatherview', ['weather' => $response->json()]);
+        } else {
+            dd('The city is not exists, please search another city.');
+        }
+       
     }
 }
+
