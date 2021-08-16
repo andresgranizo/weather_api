14307b0ab0f5e08da3d034d8a1d58bc17cd8cc1f
andresgranizo <53346123+andresgranizo@users.noreply.github.com>
2021-08-16 11:16:35 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,4 @@ use Illuminate\Support\Facades\Http;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/{dato}', function ($dato) {
-    $apiKey = config('services.apiKey.key');
-    $response = Http::get("https://api.openweathermap.org/data/2.5/weather?q={$dato}&appid={$apiKey}&units=metric");
-    return view('weatherview', ['weather' => $response->json()]);
-});
+Route::get('/{city}', [WeatherController::class, 'ShowWeather']);
